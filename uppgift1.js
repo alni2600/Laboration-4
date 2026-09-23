dbcc088dfca289e49231ed01997e62d41cca0464
alni2600 <alni2600@student.miun.se>
2026-09-23 19:25:25 +0200

    Gjort hela uppgift 1

---
+++ uppgift1.js
@@ -0,0 +1,14 @@
+// Lösning till uppgift 1. Av Alexandra Nilsson, 2026
+
+"use strict";
+
+//skapar fyra variabler och tilldelar dem ett värde.
+let firstName = "Alexandra";
+let lastName = "Nilsson";
+let age = 34;
+let isStudent = true;
+
+// Skriver ut all information
+console.log(firstName + " " + lastName);
+console.log("Ålder " + age);
+console.log(`Student: ${isStudent}`);
