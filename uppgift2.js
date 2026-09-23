9f62271b7e14f7b85a5bd0145f427facabc422ca
alni2600 <alni2600@student.miun.se>
2026-09-23 20:59:56 +0200

    Ändrade upplägget: gjorde alla beräkningar och utskrifter i en och samma funktion istället. Parametrarna skrivs in när man anropar funktionen.

--- uppgift2.js
+++ uppgift2.js
@@ -1,16 +1,22 @@
 // Lösning till uppgift 2. Av Alexandra Nilsson, 2026
-// Jag har gjort en funktion för att inte använda globala variabelnamn
-
 "use strict";
 
-//skapar en funktion som gör beräkningarna
-function calculatePriceWithoutVAT (price, quantity){
-    //Skriver ut värdena varje gång man anropar funktionen
-    console.log("Pris: " + price);
-    console.log("Antal: " + quantity);
-    return price * quantity;
+//skapar en funktion som gör beräkningarna på totalsumman med och utan moms
+function calculatePrice (price, quantity){
+     //Skriver ut värdena varje gång man anropar funktionen
+     console.log("Pris: " + price + " kr");
+     console.log("Antal: " + quantity);
+    
+     //skapar variable att lagra totalpriset i (utan moms)
+     let totPriceExclVAT = price * quantity;
+     //skriver ut totalpris utan moms
+     console.log("Totalpris utan moms: " + totPriceExclVAT + " kr");
+
+     //skapar variabel med moms och skriver ut resultatet
+     let totPriceInclVAT = totPriceExclVAT *  1.25;
+     console.log("Totalpris inkl. moms: " + totPriceInclVAT + " kr");
 }
 
-/*skapar en variabel som får priset utan moms. 
- Anropar funktionen och skriver in värden */
-let totPriceWithoutVAT = calculatePriceWithoutVAT(100, 3);
+//  Anropar funktionen och skriver in parametrarna */
+calculatePrice(100, 3);
+
