e75657291ccbacc0f23ed5a5a546c9134a6e1fc4
alni2600 <alni2600@student.miun.se>
2026-09-24 07:32:00 +0200

    uppgift 4: lade till en for-loop med villkor, bara jämna siffror mellan 1 och 20 skrivs ut

--- uppgift4.js
+++ uppgift4.js
@@ -3,6 +3,15 @@
 "use strict";
 
 //skapar en for-lop som skriver ut alla heltal från 1 till 20
+console.log("Alla heltal från 1-20:");
 for(let i = 1; i <= 20; i++){
     console.log(i);
+}
+
+//nu ska endast jämna siffror skrivas ut. Samma for-loop, men ett villkor inuti
+console.log("Alla jämna heltal från 1-20:")
+for(let i = 1; i <= 20; i++){
+   if (i % 2 === 0){
+        console.log(i);
+   }
 }
