11cde8f0fde9b71203f5fdba44993c5239650a2d
alni2600 <alni2600@student.miun.se>
2026-09-24 08:38:21 +0200

    skapade uppgift6.js. gjorde funktion som beräknade arean och anropade funktionen (värdet på parametrarna matas in där). Testkörde tre gånger med olika parametrar varje gång.

---
+++ uppgift6.js
@@ -0,0 +1,12 @@
+// Lösning till uppgift 6. Av Alexandra Nilsson, 2026
+
+"use strict";
+
+//skapar funktionen
+function calculateArea (width, height){
+    return width * height;
+}
+
+//anropar funktionen där man också skriver in värdet på parametrarna.
+// skriver ut resultatet.
+console.log("Arean är " + calculateArea(20, 30) );
