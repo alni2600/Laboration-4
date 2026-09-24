d4be55d0a22e03a1576db60dc5ca954c9d6795d6
alni2600 <alni2600@student.miun.se>
2026-09-24 07:26:34 +0200

    skapade uppgift 4.js och löste första delen av uppgiften, att skapa en for-loop som skriver ut alla tal från 1-20.

---
+++ uppgift4.js
@@ -0,0 +1,8 @@
+// Lösning till uppgift 4. Av Alexandra Nilsson, 2026
+
+"use strict";
+
+//skapar en for-lop som skriver ut alla heltal från 1 till 20
+for(let i = 1; i <= 20; i++){
+    console.log(i);
+}
