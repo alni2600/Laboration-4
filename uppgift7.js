4e52446708cd6f0de7575059e5bf1490b147e968
alni2600 <alni2600@student.miun.se>
2026-09-24 08:48:04 +0200

    skapar uppgift7.js. skapar array och en funktion med variabel och returvärde. Returvärdet blev arrayens första element, eftersom jag först ville testa att arrayen "togs emot" av funktionen och returnerade ett värde.

---
+++ uppgift7.js
@@ -0,0 +1,15 @@
+// Lösning till uppgift 7. Av Alexandra Nilsson, 2026
+
+"use strict";
+
+//skapar array med sex tal
+let numberArray = [9, 3, 7, 3, 8, 2];
+
+//skapar funktion innehållandes en variabel och ett returvärde. testkör
+function calculateSum ( ){
+    let sum = numberArray[0];
+    return sum;
+}
+
+//anropar funktionen
+console.log(calculateSum( numberArray ));
