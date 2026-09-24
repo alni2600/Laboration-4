bd1102e255d9eeda3bdcc6b0106ff0e15a4d79a8
alni2600 <alni2600@student.miun.se>
2026-09-24 07:42:08 +0200

    skapade uppgift5.js, skapade en array med fem maträtter, skrev ut hela arrayen, första och sista elementet i arrayen

---
+++ uppgift5.js
@@ -0,0 +1,15 @@
+// Lösning till uppgift 5. Av Alexandra Nilsson, 2026
+
+"use strict";
+
+//skapar array med fem maträtter
+let dishes = ["Korv stroganoff", "Blodpudding", "Spaghetti med köttfärssås", "Fisksoppa", "Kåldolmar med potatis"];
+
+//1. skriver ut alla maträtter
+console.log(`Alla maträtter: ${dishes}`);
+
+//2. skriver ut det första elementet i arrayen
+console.log(`Första elementet: ${dishes[0]}`);
+
+//3. Skriver ut sista elementet, oavsett hur många element det innehåller
+console.log(`Sista elementet: ${dishes[dishes.length-1]}`);
