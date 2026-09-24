034de822d6c3d96184cce5b8c9c5973f048e6af8
alni2600 <alni2600@student.miun.se>
2026-09-24 07:56:15 +0200

    uppgift 5, la till ett nytt element sist i arrayen, tog bort första elementet och skrev ut alla elementen i arrayen igen.

--- uppgift5.js
+++ uppgift5.js
@@ -5,7 +5,9 @@
 //skapar array med fem maträtter
 let dishes = ["Korv stroganoff", "Blodpudding", "Spaghetti med köttfärssås", "Fisksoppa", "Kåldolmar med potatis"];
 
-//1. skriver ut alla maträtter
+//1. skriver ut alla maträtter 
+//Man kan också skriva ut alla enskilda element genom att gå igenom dem i en for-loop
+//ett tredje alternativ hade varit att använda metoden forEach() och gå igenom alla element
 console.log(`Alla maträtter: ${dishes}`);
 
 //2. skriver ut det första elementet i arrayen
@@ -13,3 +15,15 @@ console.log(`Första elementet: ${dishes[0]}`);
 
 //3. Skriver ut sista elementet, oavsett hur många element det innehåller
 console.log(`Sista elementet: ${dishes[dishes.length-1]}`);
+
+//4. Lägger till en ny maträtt sist i arrayen
+dishes.push("Köttbullar med mos");
+
+//5. Tar bort den första maträtten i arrayen
+dishes.shift();
+
+//6. Skriver ut arrayen igen. Nu använder jag istället forEach()
+console.log("Skriver ut alla maträtter efter ändringarna: ");
+dishes.forEach(item => {
+    console.log(item);
+});
